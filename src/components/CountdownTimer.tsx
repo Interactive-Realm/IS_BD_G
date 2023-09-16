d24374539de623ad45ea7522157765477af1cf4d
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2023-09-17 00:05:12 +0200

    CountdownTimer

---
+++ src/components/CountdownTimer.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const CountdownTimer = () => {
+    const [count, setCount] = useState(2);
+    const [message, setMessage] = useState('');
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        if (count > -1) {
+          setCount(count - 1);
+        } else {
+          clearInterval(interval);
+          
+        }
+      }, 1000);
+  
+      return () => {
+        clearInterval(interval);
+      };
+    }, [count]);
+  
+    useEffect(() => {
+      if (count === 2) {
+        setMessage('KLAR');
+      } else if (count === 1) {
+        setMessage('PARAT');
+      }
+      else if (count === 0) {
+        setMessage('SPIL');
+      }
+      else if (count === -1) {
+        setMessage("");
+      }
+    }, [count]);
+  
+    return (
+      <div>
+        {<p>{message}</p>}
+      </div>
+    );
+  };
+
+export default CountdownTimer;
