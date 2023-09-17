68485161e634856760113a6928b8fc6521b771f8
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2023-09-17 18:16:31 +0200

    Added countdown sound

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
+import countdownSound from "/sounds/CountdownBeep.wav";
+import countdownSoundHighNode from "/sounds/CountdownBeep_HighNode.wav";
 
-const CountdownTimer = () => {
-  const [count, setCount] = useState(2);
+const CountdownTimer: React.FC = () => {
+  const [count, setCount] = useState<number>(3);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (count > -1) {
         setCount(count - 1);
+        // Play a sound based on the count
+        if (count === 3) {
+          const audioElement = new Audio(countdownSound);
+          setAudio(audioElement);
+        } else if (count === 2) {
+          const audioElement = new Audio(countdownSound);
+          setAudio(audioElement);
+        } else if (count === 1) {
+          const audioElement = new Audio(countdownSoundHighNode);
+          setAudio(audioElement);
+        }
       } else {
         clearInterval(interval);
       }
@@ -16,6 +30,15 @@ const CountdownTimer = () => {
       clearInterval(interval);
     };
   }, [count]);
+
+  useEffect(() => {
+    if (audio) {
+      audio.play().then(() => {
+        // Remove the audio element after playing
+        setAudio(null);
+      });
+    }
+  }, [audio]);
 
   let content;
   if (count === 2) {
