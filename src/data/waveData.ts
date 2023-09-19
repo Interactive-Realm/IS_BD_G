9fe59d16ebbacaa2d9cefc83481326f6ec2ebdcc
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2023-09-19 15:38:35 +0200

    changed intervals RAM

--- src/data/waveData.ts
+++ src/data/waveData.ts
@@ -7,21 +7,21 @@ export type Wave = {
 
 export const slowWave: Wave = {
   duration: 10,
-  spawnInterval: 250,
+  spawnInterval: 300,
   minBalloonSpeed: 0.12,
   maxBalloonSpeed: 0.16,
 };
 
 export const mediumWave: Wave = {
   duration: 10,
-  spawnInterval: 175,
+  spawnInterval: 225,
   minBalloonSpeed: 0.3,
   maxBalloonSpeed: 0.36,
 };
 
 export const fastWave: Wave = {
   duration: 5,
-  spawnInterval: 125,
+  spawnInterval: 150,
   minBalloonSpeed: 0.42,
   maxBalloonSpeed: 0.48,
 };
