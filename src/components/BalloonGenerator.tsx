819abda0912b6e85269f7e7f17e76325e967da77
magnus kofoed <magnus@kofoed2010.dk>
2023-09-19 17:05:24 +0200

    Merge remote-tracking branch 'origin/main' into main

--- src/components/BalloonGenerator.tsx
+++ src/components/BalloonGenerator.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, /*useMemo*/ useState } from 'react';
 import { Wave } from '../data/waveData';
 import { Balloon as BalloonType } from '../types';
 import Balloon from './Balloon';
