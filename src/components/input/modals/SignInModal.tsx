30db1c71b90e52ca628ace5530a9b18275035774
Christian <CStochholm@kami-io.com>
2023-09-20 01:15:38 +0200

    more

--- src/components/input/modals/SignInModal.tsx
+++ src/components/input/modals/SignInModal.tsx
@@ -1,16 +1,11 @@
 import Modal, { ModalProps } from '../../utils/Modal';
-import SignInForm from '../forms/SignInForm';
 
 type Props = {
-  onSignIn: () => void;
 } & ModalProps;
 
-const SignInModal = ({ isShown, setIsShown, onSignIn }: Props) => {
+const SignInModal = ({ isShown, setIsShown, }: Props) => {
   return (
     <Modal isShown={isShown} setIsShown={setIsShown}>
-      <h2>LOG IND</h2>
-      <SignInForm onSignIn={onSignIn} />
-      <button className="button text secondary">OPRET BRUGER</button>
     </Modal>
   );
 };
